a73e7be7ff8193f10077faf738a0d9c6aa2868ff
Raditz Farhan <raditzfarhan@gmail.com>
2026-07-22 14:35:30 +0800

    feat(logging): log successful CourierHttpClient calls

--- src/Http/CourierHttpClient.php
+++ src/Http/CourierHttpClient.php
@@ -4,6 +4,7 @@ namespace Laraditz\Courier\Http;
 
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
+use Laraditz\Courier\Logging\ApiLogWriter;
 use LogicException;
 
 class CourierHttpClient
@@ -18,6 +19,11 @@ class CourierHttpClient
 
     private ?string $waybillNumber = null;
 
+    public function __construct(private ?ApiLogWriter $writer = null)
+    {
+        $this->writer ??= new ApiLogWriter();
+    }
+
     public function forLog(string $driver, string $action, ?string $reference = null, ?string $waybillNumber = null): static
     {
         $this->configured = true;
@@ -29,39 +35,65 @@ class CourierHttpClient
         return $this;
     }
 
-    public function get(string $url, array $query = []): Response
+    public function get(string $url, array $query = [], array $headers = []): Response
     {
-        $this->guard();
-
-        return Http::get($url, $query);
+        return $this->send('get', $url, $query, $headers);
     }
 
-    public function post(string $url, array $data = []): Response
+    public function post(string $url, array $data = [], array $headers = []): Response
     {
-        $this->guard();
-
-        return Http::post($url, $data);
+        return $this->send('post', $url, $data, $headers);
     }
 
-    public function put(string $url, array $data = []): Response
+    public function put(string $url, array $data = [], array $headers = []): Response
     {
-        $this->guard();
-
-        return Http::put($url, $data);
+        return $this->send('put', $url, $data, $headers);
     }
 
-    public function patch(string $url, array $data = []): Response
+    public function patch(string $url, array $data = [], array $headers = []): Response
     {
-        $this->guard();
-
-        return Http::patch($url, $data);
+        return $this->send('patch', $url, $data, $headers);
     }
 
-    public function delete(string $url, array $data = []): Response
+    public function delete(string $url, array $data = [], array $headers = []): Response
+    {
+        return $this->send('delete', $url, $data, $headers);
+    }
+
+    private function send(string $method, string $url, array $data, array $headers): Response
     {
         $this->guard();
 
-        return Http::delete($url, $data);
+        $start = microtime(true);
+
+        $response = Http::withHeaders($headers)->{$method}($url, $data);
+
+        $this->log($method, $url, $headers, $data, $response, (int) round((microtime(true) - $start) * 1000));
+
+        return $response;
+    }
+
+    private function log(string $method, string $url, array $headers, array $data, Response $response, int $durationMs): void
+    {
+        if (! config('courier.logging.enabled', true)) {
+            return;
+        }
+
+        $this->writer->record([
+            'driver' => $this->driver,
+            'action' => $this->action,
+            'reference' => $this->reference,
+            'waybill_number' => $this->waybillNumber,
+            'method' => strtoupper($method),
+            'url' => $url,
+            'request_headers' => $headers,
+            'request_body' => $data,
+            'status_code' => $response->status(),
+            'response_headers' => $response->headers(),
+            'response_body' => $response->body(),
+            'duration_ms' => $durationMs,
+            'successful' => $response->successful(),
+        ]);
     }
 
     private function guard(): void
